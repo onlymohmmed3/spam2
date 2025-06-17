ae8bdbf30c8c47e4746dab00f2d5c7bd9cc59d97
onlymohmmed3 <mm86434.6.80@gmail.com>
2025-06-17 05:36:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 process.on('unhandledRejection', console.error);
-process.on('uncaughException', console.error);
+process.on('uncaughtException', console.error);
 
 const schedule = require('node-schedule');
-
-const restartJob = schedule.scheduleJob('*/5 * * * *', function() {
-    console.log('Restarting the project...');
-    // إضافة الكود الخاص بإعادة تشغيل البرنامج هنا
-});
-
 const Discord = require("discord.js-selfbot-v13");
-const client = new Discord.Client({
-  intents: [Discord.Intents.FLAGS.GUILDS],
-});
-client.on("ready", async () => {
-  console.log(`${client.user.username} is ready!`);
-});
 const { userAccount } = require("sphinx-run");
-new userAccount(client, Discord).leveling({
-  channel: "1246427655855804477",
-  randomLetters: false,
-  time: 10000, //الوقت
-  type: "ar", //الغةا
+
+// إعادة تشغيل كل 5 دقائق
+const restartJob = schedule.scheduleJob('*/5 * * * *', function() {
+  console.log('Restarting the project...');
+  // إضافة الكود الخاص بإعادة تشغيل البرنامج هنا إن لزم
 });
 
-new userAccount(client, Discord).leveling({
-  channel: "1246427655855804477",
-  randomLetters: false,
-  time: 10000, //الوقت
-  type: "eng", //الغةا
-});
+// معلومات الحسابات
+const tokens = [
+  process.env.TOKEN1,
+  process.env.TOKEN2
+];
 
-//client.login("");
-client.login(process.env.token);
+// دالة تشغيل بوت لكل توكن
+function startBot(token, type = "ar") {
+  const client = new Discord.Client({
+    checkUpdate: false,
+    intents: [Discord.Intents.FLAGS.GUILDS],
+  });
 
+  client.on("ready", async () => {
+    console.log(`${client.user.username} is ready!`);
+  });
+
+  const account = new userAccount(client, Discord);
+  account.leveling({
+    channel: "1246427655855804477",
+    randomLetters: false,
+    time: 10000,
+    type,
+  });
+
+  client.login(token);
+}
+
+// تشغيل الحساب الأول بالعربية والثاني بالإنجليزية
+startBot(tokens[0], "ar");
+startBot(tokens[1], "eng");
+
+// السيرفر الصغير لتشغيل البوت دائماً على المنصات المجانية
 const express = require("express");
 const app = express();
 var listener = app.listen(process.env.PORT || 2000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
 app.get("/", (req, res) => {
   res.send(`
   <body>
-  <center><h1>Bot 24H ON!</h1></center
+  <center><h1>Bot 24H ON!</h1></center>
   </body>`);
 });
